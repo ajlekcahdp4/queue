49fbd217228f836edae05338ed3323fe40be3391
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-02-01 18:48:05 +0300

    can find some errors

--- queue.c
+++ queue.c
@@ -2,29 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void QueCtor(Queue* que, unsigned int capacity)
+void QCtor(Queue* que, unsigned int capacity, char* Qname)
 {
     que->data = (int*)calloc (capacity, sizeof(int));
     que->capacity = capacity;
     que->bp = 0;
     que->fp = 0;
+    que->logfile = fopen (Qname, "w");
 }
 
-void QuePushB (Queue* que, int val)
+void QPushB (Queue* que, int val)
 {
     que->data[que->bp] = val;
     que->bp += 1;
+    QCheck (que);
 }
 
-void QuePopF (Queue* que, int* dest)
+void QPopF (Queue* que, int* dest)
 {
     *dest = que->data[que->fp];
     que->data[que->fp] = 0;
     que->fp += 1;
+    QCheck (que);
 }
 
-void QueDtor (Queue* que)
+void QDtor (Queue* que)
 {
+    fclose (que->logfile);
     free (que->data);
     free (que);
+}
+
+int QCheck (Queue* que)
+{
+    if (que->fp > que->bf)
+        fprintf(que->logfile, "ERROR: queue underflow\n");
+    if (que->fp < 0 )
+        fprintf (que->logfile, "ERROR: wrong front pointer\n");
+    if (que->bf < 0)
+        fprintf (que->logfile, "ERROR: wrong back pointer\n");
+        
 }
